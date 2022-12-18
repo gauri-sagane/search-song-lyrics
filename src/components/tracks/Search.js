6934e7b561261a13e2a98176a11473f2ef4795c1
Gauri <2016.gauri.sagane@ves.ac.in>
2022-12-18 12:20:11 -0600

    Add code to get searched tracks using api

--- src/components/tracks/Search.js
+++ src/components/tracks/Search.js
@@ -7,21 +7,39 @@ class Search extends Component {
         trackTitle: ''
     };
 
+    findTrack = (dispatch, e) => {
+        e.preventDefault();
+
+        axios.get(`https://api.musixmatch.com/ws/1.1/track.search?q_track=${this.state.trackTitle}&page_size=10&page=1&s_track_rating=desc&apikey=${process.env.REACT_APP_MM_KEY}`
+        )
+        .then(res => {
+            // console.log(res.data)
+            dispatch({
+                type: 'SEARCH_TRACKS',
+                payload: res.data.message.body.track_list
+            });
+
+            this.setState({trackTitle: ''});
+        })
+        .catch(err => console.log(err))
+    }
+
     onChange = e => {
-        this.setState({[e.taregt.name]: e.target.value})
+        this.setState({[e.target.name]: e.target.value})
     }
 
     render() {
         return (
             <Consumer>
                 {value => {
+                    const {dispatch} = value;
                     return (
                         <div className="card card-body mb-4 p-4">
                             <h1 className="display-4 text-center">
                             <i className="fa fa-music"> Search For a Song</i>
                             </h1>
                             <p className="lead text-center">Get the Lyrics for any Song</p>
-                            <form>
+                            <form onSubmit={this.findTrack.bind(this, dispatch)}>
                                 <div className="form-group">
                                     <input type="text" className="form-control form-control-lg" placeholder="Song title..." name="trackTitle" value={this.state.trackTitle} onChange={this.onChange} />
                                 </div>
